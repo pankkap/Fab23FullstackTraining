9366b8ce3d37a5b043cf950f28d00dfa49a6e68c
pankkap <pankkap@gmail.com>
2023-03-19 21:21:04 +0530

    Session-10 COde

---
+++ Session-10/HelloWorld.js
@@ -0,0 +1,13 @@
+// variable: It is used to store temporary data in Every Programming Lanuage
+
+var x = 125
+var y = 73
+var add = x + y
+var sub = x - y
+var mul = x * y
+var div = x / y
+console.log("The Result of X + y ", add)
+console.log("The Result of X - y ", sub)
+console.log("The Result of X * y ", mul)
+console.log("The Result of X / y ", div)
+
---
+++ Session-10/takingInput.js
@@ -0,0 +1,7 @@
+var name = prompt("Enter your Name")
+var age = Number( prompt("Enter your Age"))
+var city = prompt("Enter your city")
+
+document.write("My Name :", name)
+document.write("<br>My Age  :", age)
+document.write("<br>My City  :", city)
