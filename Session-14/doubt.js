08409d83ce90089f58a9843bcbccc0065b300323
pankkap <pankkap@gmail.com>
2023-04-08 21:20:05 +0530

    Session-15 Code

---
+++ Session-14/doubt.js
@@ -0,0 +1,17 @@
+// Function and Loop
+
+// SUm of five number send to the function
+
+
+function printTable(num)
+{
+    for (i=1;i<=10;i++)
+    {
+        console.log(num + " x "+i+ " = " + num*i)
+    }
+}
+
+printTable(6)
+
+// 6 x 1 = 6
+// 6 x 2 = 12
