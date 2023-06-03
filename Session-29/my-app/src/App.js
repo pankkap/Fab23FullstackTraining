815ca30cc359f0bdae95915dba05428e5a3de6b7
pankkap <pankkap@gmail.com>
2023-06-03 21:24:40 +0530

    React App

---
+++ Session-29/my-app/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>My React Application</h2>
+        <p> 
+        Pankaj Kapoor
+        </p>
+        <a
+          className="App-link"
+          href="https://beta-labs.in"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
